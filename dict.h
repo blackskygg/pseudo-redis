b4ecb60cfbaf45a9c4954fc1456be04dbe29a313
Sky Liu <blackskygg@gmail.com>
2015-12-02 18:40:21 +0800

    the implementation of dictionary(hash table); but resizing is not yet supported

---
+++ dict.h
@@ -0,0 +1,67 @@
+#ifndef DICT_H_
+#define DICT_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "obj.h"
+#include "bss.h"
+#include "err.h"
+
+#define MAX_POWER 32
+#define MAX_UINT32 0xffffffff
+
+struct dict_entry {
+        bss_t *key;
+        obj_t *val;
+        struct dict_entry *next;
+};
+typedef struct dict_entry dict_entry_t;
+
+/* a backward iterator, which will make it easier to perform deletion */
+struct dict_iter {
+        dict_entry_t *curr;
+        dict_entry_t *prev;
+};
+typedef struct dict_iter dict_iter_t;
+
+struct dict {
+        dict_entry_t **hash_tbl;
+
+#ifdef DICT_RESIZE
+        dict_entry_t **hash_tbl2;
+#endif
+        uint8_t power;
+        size_t entry_num;
+        uint32_t bit_mask;
+};
+
+typedef struct dict dict_t;
+/* we use a 32-bit unsigned int as hash key */
+typedef uint32_t hkey_t;
+/* prototype of the callback function required by dict_iter() */
+typedef void (*dict_callback_t)(const dict_iter_t *dict_iter);
+
+/* convert power to uint32_t */
+#define pwr2size(power) (((uint32_t)1) << (power))
+
+/* dict APIs */
+dict_t *dict_create(int power);
+void dict_destroy(dict_t *dict);
+dict_entry_t *dict_entry_create(const bss_t *key, obj_t *obj);
+uint32_t dict_get_hash(const bss_t *bss);
+int dict_add(dict_t *dict, const bss_t *key, obj_t *obj);
+int dict_rm(dict_t *dict, const bss_t *key);
+obj_t *dict_look_up(const dict_t *dict, const bss_t *key);
+void dict_iter(const dict_t *dict, dict_callback_t func);
+
+#ifdef DICT_RESIZE
+int dict_expand(dict_t *dict, uint8_t power);
+#endif
+
+
+/* obj-layer dict APIs */
+obj_t *dict_create_obj(const void *data);
+void dict_destroy_obj(obj_t *dict_obj);
+
+
+#endif
--- main.c
+++ main.c
@@ -1,27 +1,37 @@
 #include "bss.h"
+#include "dict.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
         bss_t* bss;
+        obj_t* obj;
         obj_t* arr[1024];
+        dict_t *dict =  dict_create(3);
 
         bss = malloc(sizeof(bss_t) + 1024);
-        for(int i = 0; i < 100; ++i) {
+        for(int i = 0; i < 1000; ++i) {
                 bss->len = sprintf(bss->str, "%dsssssssssss%d", i, i) + 1;
                 arr[i] = bss_create_obj(bss);
         }
-
-        for(int i = 0; i < 40; ++i)
-                arr[i] = arr[i]->op->cpy(arr[i], arr[i+40]);
-        for(int i = 0; i < 100; ++i){
-                printf("%s\n", ((bss_t *)arr[i]->val)->str);
+        for(int i = 0; i < 1000; ++i){
+                bss->len = sprintf(bss->str, "%d", i) + 1;
+                dict_add(dict, bss, arr[i]);
         }
 
-        for(int i = 0; i < 100; ++i)
-                arr[i]->op->destroy(arr[i]);
+        for(int i = 0; i < 1000; i+=2) {
+                bss->len = sprintf(bss->str, "%d", i) + 1;
+                dict_rm(dict, bss);
+        }
 
+        for(int i = 0; i < 1000; ++i){
+                bss->len = sprintf(bss->str, "%d", i) + 1;
+                if(NULL != (obj = dict_look_up(dict, bss)))
+                        printf("%s\n", ((bss_t *)obj->val)->str);
+        }
+
+        dict_destroy(dict);
         free(bss);
 
         return 0;
